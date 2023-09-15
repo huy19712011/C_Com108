08e5e41f4b52cea79dfbd258b259930219d5ba6e
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2023-09-15 21:52:16 +0700

    size of data types

---
+++ part1/e04_sizeOfTypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("size of char = %d\n", sizeof(char));
+    printf("size of short = %d\n", sizeof(short));
+    printf("size of int = %d\n", sizeof(int));
+    printf("size of unsigned int = %d\n", sizeof(unsigned int));
+    printf("size of float = %d\n", sizeof(float));
+    printf("size of double = %d\n", sizeof(double));
+    printf("size of long double = %d\n", sizeof(long double));
+
+
+    return 0;
+}
